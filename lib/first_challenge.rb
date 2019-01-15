f6c8e22f1108e44d226d4ff21ea91a00b9636242
Samidh Patel <spatel13456@gmail.com>
2019-01-15 20:15:36 +0000

    Automatically backed up by Learn

--- lib/first_challenge.rb
+++ lib/first_challenge.rb
@@ -18,6 +18,7 @@ def first_challenge
     if attribute == :favorite_icecream_flavors
         value.delete_if {|flavor| flavor = "strawberry"}
   #remember to return your newly altered contacts hash!
+      end
     end
   end
 end
