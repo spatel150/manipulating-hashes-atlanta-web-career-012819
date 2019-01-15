c0263f51c7a36a4840b468e1a55f987e600031b8
Samidh Patel <spatel13456@gmail.com>
2019-01-15 20:00:44 +0000

    Automatically backed up by Learn

--- lib/first_challenge.rb
+++ lib/first_challenge.rb
@@ -15,7 +15,7 @@ def first_challenge
 
   contacts.each do |person, data|
   
-  data.each do |attribute, |
+  data.each do |attribute, value|
 
   #remember to return your newly altered contacts hash!
   contacts
